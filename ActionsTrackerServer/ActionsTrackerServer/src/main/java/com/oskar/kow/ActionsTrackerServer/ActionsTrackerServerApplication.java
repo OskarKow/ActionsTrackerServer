0cb91ab53dd6ac20c6d35252ec5a73abd36d2ac8
Kowalewski <s160359@student.pg.gda.pl>
2017-07-26 18:01:01 +0200

    Created project, created first DB table - companies.

---
+++ ActionsTrackerServer/ActionsTrackerServer/src/main/java/com/oskar/kow/ActionsTrackerServer/ActionsTrackerServerApplication.java
@@ -0,0 +1,12 @@
+package com.oskar.kow.ActionsTrackerServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsTrackerServerApplication.class, args);
+	}
+}
